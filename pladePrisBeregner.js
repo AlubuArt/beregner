549dc79c357c05788f13f286891241005bce3820
AlubuArt <57637214+AlubuArt@users.noreply.github.com>
2020-08-21 10:49:39 +0200

    css done

--- pladePrisBeregner.js
+++ pladePrisBeregner.js
@@ -3,36 +3,56 @@ var product = {};
 
 function buttonClick() {
 
-    var pricePrM2 = 150;
-
-   //get the selected type
-    var selectedType = eval(document.getElementById("selectType").value);  
-
-   // get the selected width
-    var selectedLength = document.getElementById("selectedLength").value;
-    var selectedWidth = document.getElementById("selectedWidth").value;
+   
+   var selectedType = eval(document.getElementById("selectType").value);  
+   var selectedLength = document.getElementById("selectedLength").value;
+   var selectedWidth = document.getElementById("selectedWidth").value;
     
 
-   // calculate the price
-    var calculatedPrice = Math.floor(selectedLength * selectedType.price * selectedWidth);
-    var calculatedM2 = (selectedWidth * selectedLength / 1000000);
+   
+   var productPrice = calculatePrice(selectedLength, selectedType, selectedWidth);
+   var productM2 = calculateM2(selectedLength, selectedWidth);
+   var productWeight = calculateWeight(selectedType, productM2);
+
 
    //construct a product
-   product = {type: selectedType, 
-                  price: calculatedPrice, 
-                  width: selectedWidth,
-                  lenght: selectedLength,
-                  sizem2: calculatedM2
-                  };
+   product = { type: selectedType, 
+               price: productPrice, 
+               width: selectedWidth,
+               lenght: selectedLength,
+               sizem2: productM2,
+               weight: productWeight
+               };
 
     //show the result
-    document.getElementById('resultPrice').innerHTML= '<p>Pris inkl. moms: ' + product.price + '</p>';
+    document.getElementById('resultPrice').innerHTML= '<p>Pris inkl. moms: ' + product.price + ' kr </p>';
     document.getElementById('resultSize').innerHTML= '<p>Pladens størrelse: ' + product.sizem2 + ' m2 </p>';
+    document.getElementById('resultWeight').innerHTML = '<p>Pladens vægt: ' + productWeight + ' kg </p>';
     
     
-    console.log(product.sizem2);
+    console.log(product)
     
 }
+
+function calculateWeight(selectedType, productM2) {
+
+   var calculatedWeight = (selectedType.weight * productM2)
+   return calculatedWeight;
+}
+
+
+function calculatePrice(selectedLength, selectedType, selectedWidth){
+
+   var calculatedPrice = Math.floor(selectedLength * selectedType.price * selectedWidth);
+   return calculatedPrice;
+}
+
+
+function calculateM2(selectedLength, selectedWidth){
+   var calculatedM2 = (selectedWidth * selectedLength / 1000000);
+   return calculatedM2;
+}
+
 
 function addProduct() {
 
---
+++ pladeTykkelser.js
@@ -0,0 +1,29 @@
+var fourMm = {
+    price: 0.000649,
+    name: "4mm",
+    weight: 2   
+};
+
+var sevenMm = {
+    price: 0.000782,
+    name: "7mm",
+    weight: 3
+}
+
+var nineMm = {
+    price: 0.000925,
+    name: "9mm",
+    weight: 5
+}
+
+var twelveMm = {
+    price: 0.001073,
+    name: "12mm",
+    weight: 6
+}
+
+var sixteenMm = {
+    price: 0.001222,
+    name: "16mm",
+    weight: 8,
+}
