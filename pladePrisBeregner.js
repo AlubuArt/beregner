d9f85de33982b3a6fa2343ce617005b77dd9c720
AlubuArt <57637214+AlubuArt@users.noreply.github.com>
2020-08-24 13:43:23 +0200

    Beregneren DONE

--- pladePrisBeregner.js
+++ pladePrisBeregner.js
@@ -1,42 +1,54 @@
 
+var productPrice = 0;
+var selectedType = "4mm";
+var selectedLength = 0;
+var selectedWidth = 0;
+var productM2 = 0;
+var productWeight = 0;
+var fragtPris = 0;
 var product = {};
+
+
 
 function buttonClick() {
 
    
-   var selectedType = eval(document.getElementById("selectType").value);  
-   var selectedLength = document.getElementById("selectedLength").value;
-   var selectedWidth = document.getElementById("selectedWidth").value;
-    
+   selectedType = eval(document.getElementById("selectType").value);  
+   selectedLength = document.getElementById("selectedLength").value;
+   selectedWidth = document.getElementById("selectedWidth").value;
+   productPrice = calculatePrice(selectedLength, selectedType, selectedWidth);
+   productM2 = calculateM2(selectedLength, selectedWidth);
+   productWeight = calculateWeight(selectedType, productM2);
+   fragtPris = friFragt(productPrice, productWeight, selectedLength, selectedWidth)
 
-   
-   var productPrice = calculatePrice(selectedLength, selectedType, selectedWidth);
-   var productM2 = calculateM2(selectedLength, selectedWidth);
-   var productWeight = calculateWeight(selectedType, productM2);
-
-
-   //construct a product
-   product = { type: selectedType, 
+   product = { 
+               name: "Marine Plywood",
+               type: selectedType, 
                price: productPrice, 
                width: selectedWidth,
                lenght: selectedLength,
                sizem2: productM2,
-               weight: productWeight
+               weight: productWeight,
+               fragt: fragtPris
                };
+   
 
     //show the result
-    document.getElementById('resultPrice').innerHTML= '<p>Pris inkl. moms: ' + product.price + ' kr </p>';
-    document.getElementById('resultSize').innerHTML= '<p>Pladens størrelse: ' + product.sizem2 + ' m2 </p>';
-    document.getElementById('resultWeight').innerHTML = '<p>Pladens vægt: ' + productWeight + ' kg </p>';
+   document.getElementById('resultPrice').innerHTML= '<p>Pris inkl. moms: ' + product.price + ' kr </p>';
+   document.getElementById('resultSize').innerHTML= '<p>Pladens størrelse: ' + product.sizem2 + ' m2 </p>';
+   document.getElementById('resultWeight').innerHTML = '<p>Pladens vægt: ' + productWeight + ' kg </p>';
+   document.getElementById('fragt').innerHTML= '<p>' + fragtPris + '</p>';
+   //construct a product
+   
+   return product;
     
-    
-    console.log(product)
+   
     
 }
 
 function calculateWeight(selectedType, productM2) {
 
-   var calculatedWeight = (selectedType.weight * productM2)
+   var calculatedWeight = (selectedType.weight * productM2);
    return calculatedWeight;
 }
 
@@ -53,14 +65,31 @@ function calculateM2(selectedLength, selectedWidth){
    return calculatedM2;
 }
 
-
-function addProduct() {
-
-   confirm("Du er ved at lægge en vare i kurven som ikke kan byttes. Venligst sikre at målene er korrekt. \n\nTryk OK hvis varen er korrekt, ellers annuller" );
-
-   if (addProduct) {
-      console.log("Varen er lagt i kurven");
-      alert("Varen er lagt i kurven. Du kan nu fortsætte med at handle.")
+function friFragt(productPrice, productWeight, selectedLength, selectedWidth){
+   
+   if ( (productPrice > 500) && (productWeight <= 20) && (selectedLength <= 2000) && selectedWidth <= 2000 ) {
+      return "Fragt: Du får fri fragt!"
+   } else {
+      return"Fragt: 49kr"
    }
+   
 }
+
+
+function addToBasket(){
+   
+   var productToBeAdded = buttonClick();
+
+   document.getElementById('productName').innerHTML = '<p>' + productToBeAdded.name + ' ' + productToBeAdded.type.name + '</p>';
+   document.getElementById('productSize').innerHTML = '<p>' + productToBeAdded.sizem2 + '</p>';
+   console.log(productToBeAdded.type.name)
+}
+
+
+
+   
+
+   
+
+
 
